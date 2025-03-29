8ef144a8715b0d70547286158dfa587f2b59cb5b
dharminder <144053986+DharminderPal@users.noreply.github.com>
2025-03-29 13:48:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 ✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅
-// import React from "react";
-import React, { useState } from "react";
+import React from "react";
+// import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
