7cd6b05baa339c26035736855c789bfad1112cc5
dharminder <144053986+DharminderPal@users.noreply.github.com>
2025-03-29 12:01:26 +0530

    Update App.js
    i have change some code

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,18 @@ import './../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="app-container">
+      {/* Input Field */}
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="input-field"
+      />
+
+      {/* Greeting Display */}
+      {name && <h2 className="greeting">Hello, {name}!</h2>}
     </div>
   )
 }
