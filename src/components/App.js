5beea02eb2d9a28ff8d68c7b0f8e624a001d6497
dharminder <144053986+DharminderPal@users.noreply.github.com>
2025-03-29 13:50:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅
+
 import React from "react";
 // import React, { useState } from "react";
 import './../styles/App.css';
@@ -25,4 +25,4 @@ const App = () => {
 }
 
 export default App
-✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅
+
